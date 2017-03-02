df2388bf37e816d653337abf935846ae10a1e5a1
Kyle Winckler <kyle.winckler@gmail.com>
2017-03-01 20:25:58 -0800

    have image upload working

--- lab-kyle/app/component/gallery/thumbnail/thumbnail.js
+++ lab-kyle/app/component/gallery/thumbnail/thumbnail.js
@@ -1,10 +1,24 @@
-// 'use strict'
-//
-// module.exports = {
-//   template: require('./thumbnail.html'),
-//   controller: ['$log', 'imageService', ThumbnailController],
-//   controllerAs: thumbnailCtrl,
-//   bindings: {
-//     gallery: '<'
-//   }
-// }
+'use strict'
+
+require('./_thumbnail.scss')
+
+module.exports = {
+  template: require('./thumbnail.html'),
+  controller: ['$log', 'imageService', ThumbnailController],
+  controllerAs: 'thumbnailCtrl',
+  bindings: {
+    images: '<'
+  }
+}
+
+function ThumbnailController($log, imageService) {
+  $log.debug('ThumbnailController')
+
+  this.showAllPics = function() {
+    $log.debug('ThumbnailController.showAllPics')
+  }
+
+  this.deletePic = function() {
+    $log.debug('ThumbnailController.deletePic')
+  }
+}
